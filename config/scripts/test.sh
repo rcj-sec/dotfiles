a7e6635c95a60995227c18ea1b688773200c756f
whispers-of-enma <rcj.dev@proton.me>
2025-05-04 12:32:13 +0200

    fix: fixed starpresets

--- config/scripts/test.sh
+++ config/scripts/test.sh
@@ -6,5 +6,6 @@ shell_safe_path=$absolute_path
 if [[ "$absolute_path" == "$HOME"* ]]; then
     shell_safe_path="\$HOME${absolute_path#$HOME}"
 fi
+
 echo Absolute path:      $absolute_path
 echo Shell safe path:    $shell_safe_path
