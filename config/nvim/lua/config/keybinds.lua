fb2ccdd4412064b0ffbf7b8cb9736f9e88662c15
rcj <rcj-dev@proton.me>
2025-12-09 15:26:10 +0100

    updates

--- config/nvim/lua/config/keybinds.lua
+++ config/nvim/lua/config/keybinds.lua
@@ -2,26 +2,26 @@ vim.g.mapleader = " "
 
 local builtin = require("telescope.builtin")
 
--- alt + e = open explorer
+-- ALT + e = open explorer
 vim.keymap.set({ "n", "i", "v" }, "<M-e>", "<Esc>:Explore<CR>", { noremap = true, silent = true })
 
--- alt + n = toggle neotree
+-- ALT + n = toggle neotree
 vim.keymap.set({ "n", "i", "v" }, "<M-n>", "<Esc>:Neotree toggle<CR>", { noremap = true, silent = true })
 
--- alt + s = :w
+-- ALT + w = :w
 vim.keymap.set({ "n", "i", "v" }, "<M-w>", "<Esc>:w<CR>", { noremap = true, silent = true })
 
--- alt + q = :q
+-- ALT + q = :q
 vim.keymap.set({ "n", "i", "v" }, "<M-q>", "<Esc>:q<CR>", { noremap = true, silent = true })
 
--- alt + enter = telescope fuzzy find
-vim.keymap.set({ "n", "i", "v" }, "<M-CR>", builtin.find_files, { noremap = true, silent = true })
+-- leader + ff = telescope fuzzy find
+vim.keymap.set({ "n" }, "<leader>ff", builtin.find_files, { noremap = true, silent = true })
 
--- alt + l = telescope live grep
-vim.keymap.set({ "n", "i", "v" }, "<M-l>", builtin.live_grep, { noremap = true, silent = true })
+-- leader + lg = telescope live grep
+vim.keymap.set({ "n" }, "<leader>lg", builtin.live_grep, { noremap = true, silent = true })
 
--- alt + f = format document
-vim.keymap.set({ "n", "i" }, "<M-f>", vim.lsp.buf.format, { noremap = true, silent = true })
+-- leader + fmt = format document
+vim.keymap.set({ "n" }, "<leader>fmt", vim.lsp.buf.format, { noremap = true, silent = true })
 
 -- shift + k = lsp hover function details in normal mode
 vim.keymap.set({ "n" }, "K", vim.lsp.buf.hover, { noremap = true, silent = true })
